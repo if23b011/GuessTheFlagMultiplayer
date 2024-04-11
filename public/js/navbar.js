885d55792de86f211d11fb37765118928c5caf5c
Armin Naseri <if23b066@technikum-wien.at>
2024-04-11 13:03:24 +0200

    feature angefangen

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,12 +1,24 @@
 import {
+    collection,
+    getDocs,
+    getFirestore,
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
+import {
     getAuth,
     onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+const db = getFirestore();
 const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
+    const querySnapshot = getDocs(collection(db, "username"));
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        appendUser(doc.data().username);
+    });
+
     if (user) {
         // User is signed in.
-        document.getElementById("profile").innerText = user.email;
+        document.getElementById("profile").innerText = querySnapshot;
     } else {
         // No user is signed in.
         document.getElementById("profile").innerText = "Not logged in";
