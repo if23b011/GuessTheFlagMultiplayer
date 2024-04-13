d3806f21db63d647c004e55b669a44028e0b038e
Armin Naseri <if23b066@technikum-wien.at>
2024-04-13 14:10:35 +0200

    feature eingefügt (testing)

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,24 +1,26 @@
 import {
-    collection,
-    getDocs,
-    getFirestore,
-} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
-import {
     getAuth,
     onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
-const db = getFirestore();
-const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-    const querySnapshot = getDocs(collection(db, "username"));
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-        appendUser(doc.data().username);
-    });
+import { 
+    getFirestore,
+    doc,
+    getDoc 
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 
+const auth = getAuth();
+const db = getFirestore();
+
+onAuthStateChanged(auth, async (user) => {
     if (user) {
         // User is signed in.
-        document.getElementById("profile").innerText = querySnapshot;
+        const userDoc = await getDoc(doc(db, "users", user.uid));
+        if (userDoc.exists()) {
+            const userData = userDoc.data();
+            document.getElementById("profile").innerText = userData.username;
+        } else {
+            console.log("User data not found");
+        }
     } else {
         // No user is signed in.
         document.getElementById("profile").innerText = "Not logged in";
