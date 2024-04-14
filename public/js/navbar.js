09007e868bb3106bd4813c795d24ace8c2d7ba7d
if23b152 <161253553+if23b152@users.noreply.github.com>
2024-04-14 17:19:58 +0200

    Update navbar.js
    signout hinzugefügt: ird nur angezeigt wenn user eingeloggt ist

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,6 +1,7 @@
 import {
     getAuth,
     onAuthStateChanged,
+    signOut // Import für signOut hinzufügen
 } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
 import {
     getFirestore,
@@ -12,10 +13,8 @@ import {
 
 const auth = getAuth();
 const firestore = getFirestore();
-
 onAuthStateChanged(auth, async (user) => {
     if (user) {
-        // User is signed in.
         const q = query(
             collection(firestore, "users"),
             where("email", "==", user.email)
@@ -25,8 +24,25 @@ onAuthStateChanged(auth, async (user) => {
             const userData = doc.data();
             document.getElementById("profile").innerText = userData.username;
         });
+        document.getElementById('signOutButton').style.display = 'block';
     } else {
         // No user is signed in.
         document.getElementById("profile").innerText = "Not logged in";
+        document.getElementById('signOutButton').style.display = 'none';
+    }
+});
+
+// signOut
+document.addEventListener('DOMContentLoaded', () => {
+    const signOutBtn = document.getElementById('signOutButton');
+    if(signOutBtn){
+        signOutBtn.addEventListener('click', () => {
+            signOut(auth).then(() => {
+                console.log('User signed out.');
+                window.location.href = 'index.html?page=signIn';
+            }).catch((error) => {
+                console.error('Sign Out Error', error);
+            });
+        });
     }
 });
