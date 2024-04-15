c4c7bdf29ab1fffb087b46a5febe43d64203a256
Jan Halwax <if23b011@technikum-wien.at>
2024-04-15 12:27:10 +0200

    Sign Out überarbeitet

--- public/js/signOut.js
+++ public/js/signOut.js
@@ -1,18 +1,19 @@
-import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+import { app } from "../js/firebase-config.js";
+import {
+    getAuth,
+    signOut,
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const signOutButton = document.getElementById('signOutButton');
-  
-  if (signOutButton) {
-    signOutButton.addEventListener('click', () => {
-      const auth = getAuth();
-      
-      signOut(auth).then(() => {
-        console.log('User signed out.');
-        window.location.href = 'index.html?page=signIn';
-      }).catch((error) => {
-        console.error('Sign Out Error', error);
-      });
-    });
-  }
+const signOutButton = document.getElementById("signOutButton");
+
+signOutButton.addEventListener("click", () => {
+    const auth = getAuth(app);
+    signOut(auth)
+        .then(() => {
+            console.log("User signed out.");
+            window.location.href = "index.html?page=signIn";
+        })
+        .catch((error) => {
+            console.error("Sign Out Error", error);
+        });
 });
