cd3117f0a73156ec1133006e4b279288e83a861c
Jan Halwax <if23b011@technikum-wien.at>
2024-04-16 15:42:26 +0200

    Next Flag Button

--- public/js/game.js
+++ public/js/game.js
@@ -10,14 +10,26 @@ const db = getFirestore(app);
 const gameRef = await getDocs(collection(db, "games"));
 const game = gameRef.docs.find((doc) => doc.id === gameID).data();
 const flags = game.flags.match(/.{1,2}/g);
-flags.forEach(async (flag) => {
-    const flagRef = await getDocs(collection(db, "flags"));
-    const flagData = flagRef.docs.find((doc) => doc.data().id === flag).data();
-    const flagElement = document.createElement("div");
-    const img = document.createElement("img");
-    img.src = "../img/flags/" + flagData.name + ".png";
-    img.alt = flagData.name;
-    img.style.width = "180px";
-    document.querySelector(".flags").appendChild(flagElement);
-    flagElement.appendChild(img);
+
+const flagRef = await getDocs(collection(db, "flags"));
+const flagData = flagRef.docs.find((doc) => doc.data().id === flags[0]).data();
+
+var index = 0;
+document.getElementById("nextFlag").addEventListener("click", () => {
+    index++;
+    const nextFlagData = flagRef.docs
+        .find((doc) => doc.data().id === flags[index])
+        .data();
+    img.src = "../img/flags/" + nextFlagData.name + ".png";
+    img.alt = nextFlagData.name;
+    if (index === flags.length - 1) {
+        document.getElementById("nextFlag").disabled = true;
+    }
 });
+const flagElement = document.createElement("div");
+const img = document.createElement("img");
+img.src = "../img/flags/" + flagData.name + ".png";
+img.alt = flagData.name;
+img.style.width = "180px";
+document.querySelector(".flags").appendChild(flagElement);
+flagElement.appendChild(img);
